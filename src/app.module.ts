493c7b5ef670ec7ebe2a11c502131a271bde0856
cnazha <christian@nazha.dev>
2023-10-26 12:47:09 +0300

    feat(mongo): add mongoose with config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { envConfigOptions } from './config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { envConfigOptions, mongooseConfig } from './config';
 
 @Module({
-  imports: [ConfigModule.forRoot(envConfigOptions)],
+  imports: [
+    ConfigModule.forRoot(envConfigOptions),
+    MongooseModule.forRootAsync(mongooseConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
