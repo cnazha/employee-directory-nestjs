2f623c04fc97c150a1249210d7bb381ac4aafc11
cnazha <christian@nazha.dev>
2023-10-26 12:50:33 +0300

    feat(graphql): add graphql with config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,15 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { envConfigOptions, mongooseConfig } from './config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { graphqlConfig } from './config/graphql.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(envConfigOptions),
     MongooseModule.forRootAsync(mongooseConfig),
+    GraphQLModule.forRoot<ApolloDriverConfig>(graphqlConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
