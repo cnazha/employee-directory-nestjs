8bc4620a860c4e8695e4286f9c1cf2e971e4d193
cnazha <christian@nazha.dev>
2023-10-28 11:41:35 +0300

    feat(employees): add image entity

---
+++ src/common/entities/image.entity.ts
@@ -0,0 +1,43 @@
+import { Field, Float, ObjectType } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { BaseDocumentEntity } from './base.document';
+import { HydratedDocument } from 'mongoose';
+
+@Schema({
+  _id: false,
+  timestamps: true,
+  toJSON: { getters: true },
+})
+@ObjectType()
+export class ImageEntity extends BaseDocumentEntity {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  @Field(() => String, { nullable: false, description: 'Public image URL' })
+  url: string;
+  @Prop({
+    type: String,
+    required: true,
+  })
+  @Field(() => String, {
+    nullable: false,
+    description: 'Image path on Firebase Bucket',
+  })
+  path: string;
+  @Prop({
+    type: Number,
+    required: true,
+  })
+  @Field(() => Float, { nullable: false, description: 'Original image width' })
+  width: number;
+  @Prop({
+    type: Number,
+    required: true,
+  })
+  @Field(() => Float, { nullable: false, description: 'Original image height' })
+  height: number;
+}
+
+export type ImageDocument = HydratedDocument<ImageEntity>;
+export const ImageSchema = SchemaFactory.createForClass(ImageEntity);
