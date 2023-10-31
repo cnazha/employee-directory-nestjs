24c5c009c63486bd5eb6aee6f2f4d3179f3db1a1
cnazha <christian@nazha.dev>
2023-10-31 14:29:43 +0200

    config(helmet): update config for deployment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,26 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: [
+            `'self'`,
+            'data:',
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [
+            `'self'`,
+            'apollo-server-landing-page.cdn.apollographql.com',
+          ],
+          frameSrc: [`'self'`, 'sandbox.embed.apollographql.com'],
+        },
+      },
+    }),
+  );
   const config = app.get<ConfigService>(ConfigService);
   const port = config.get('app.port');
   await app.listen(port);
