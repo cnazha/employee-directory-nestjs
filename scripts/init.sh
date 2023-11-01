24e4afbb771fba4d762a487900c7094bdc4af177
cnazha <christian@nazha.dev>
2023-11-01 15:02:30 +0200

    fix(script): added parent path of the init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Get the parent path of this script
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
 
 # Create .env file from sample
 source ./create-env.sh
